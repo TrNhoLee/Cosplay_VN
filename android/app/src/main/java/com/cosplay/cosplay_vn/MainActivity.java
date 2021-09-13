a8b33fa6edb1fc930e98da10603991cca91b6eaa
Le_Trong_Nho <trongnholeit@gmail.com>
2021-09-13 22:58:51 +0700

    New project

---
+++ android/app/src/main/java/com/cosplay/cosplay_vn/MainActivity.java
@@ -0,0 +1,6 @@
+package com.cosplay.cosplay_vn;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:cosplay_vn/Views/MyHomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
